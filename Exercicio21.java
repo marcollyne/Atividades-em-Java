b611f2339ca669912f05e804eeed104704619687
karollyne <weelly2704@gmail.com>
2020-07-30 15:50:10 -0300

    soma de duas variaveis double

---
+++ Exercicio21.java
@@ -0,0 +1,30 @@
+/*
+ Exercicios simples de introdução de java acompanhando o livro "Desbravando Java
+ e Orientação a Objetos".
+ */
+
+/**
+ *
+ * @author karol
+ */
+
+/*2.2 Declaração e atribuição de variáveis
+Vamos criar uma nova classe Java cujo objetivo será calcular o valor
+total de livros do nosso estoque na livraria
+*/
+
+public class Exercicio21 {
+    
+    public static void main(String[] args) {
+        
+        double livroJava8 = 59.90;
+        double livroTDD = 59.90;
+        double soma = livroJava8 + livroTDD;
+        
+        System.out.println("O total em estoque é " + soma);
+        
+    }
+        
+    }
+    
+

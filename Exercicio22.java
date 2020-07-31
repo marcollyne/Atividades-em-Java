1b00bf1ecacb1bd473ebb5f446a9916a46c7e18b
karollyne <weelly2704@gmail.com>
2020-07-31 16:41:23 -0300

    utilizando looping for

--- Exercicio22.java
+++ Exercicio22.java
@@ -17,28 +17,43 @@ public class Exercicio22 {
     public static void main(String[] args) {
 
         DecimalFormat df = new DecimalFormat("0.00");
-        double soma = 0;
+        double soma1 = 0;
+        double soma2 = 0;
         int i = 0;
 
         System.out.println("usando while");
         while (i < 35) {
             double valorDoLivro = 59.90;
-            soma += valorDoLivro;
+            soma1 += valorDoLivro;
             i++;
         }
-        System.out.println("O total em estoque é: " + df.format(soma));
+        System.out.println("O total em estoque é: " + df.format(soma1));
 
-        if (soma < 150) {
+        if (soma1 < 150) {
             System.out.println("Seu estoque está muito baixo!");
-        } else if (soma >= 2000) {
+        } else if (soma1 >= 2000) {
             System.out.println("Seu estoque está muito alto!");
         } else {
             System.out.println("Seu estoque está bom (y)");
         }
-        
-        
+
         System.out.println("");
         System.out.println("usando for");
+
+        for (int j = 0; j < 35; j++) {
+            double valorDoLivro = 59.90;
+            soma2 += valorDoLivro;
+        }
+
+        System.out.println("O total em estoque é: " + df.format(soma2));
+
+        if (soma2 < 150) {
+            System.out.println("Seu estoque está muito baixo!");
+        } else if (soma2 >= 2000) {
+            System.out.println("Seu estoque está muito alto!");
+        } else {
+            System.out.println("Seu estoque está bom (y)");
+        }
     }
 
 }

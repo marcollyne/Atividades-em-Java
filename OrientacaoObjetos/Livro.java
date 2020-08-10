ea5bb786a1f2f3f779ae8ba2695fb97513520de5
karollyne <weelly2704@gmail.com>
2020-08-10 14:36:09 -0300

    iniciando orientação a objetos, criada classe livro e seus atributos

---
+++ OrientacaoObjetos/Livro.java
@@ -0,0 +1,16 @@
+/*
+iniciando orientação a objetos com o livro Desbravando Java e Orientação a Objetos
+ */
+package OrientacaoObjetos;
+
+/**
+ *
+ * @author karol
+ */
+public class Livro {
+
+    String nome;
+    String descricao;
+    double valor;
+    String isbn;
+}
